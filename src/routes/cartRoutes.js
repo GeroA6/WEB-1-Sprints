482618ce86af016c93f4bf994ec90b931d8f8777
Pablo Aguilar <paedmaag@gmail.com>
2026-04-30 01:27:16 -0300

    feat: Se agregaron las rutas correspondientes al update y remove del carrito

--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -8,6 +8,12 @@ router.get('/', cartController.viewCart);
 // La ruta real será POST /cart/add/:id
 router.post('/add/:id', cartController.addCart);
 
+// Modificar cantidad (+ o -): POST /cart/update/:id
+router.post('/update/:id', cartController.updateCart);
+
+// Quitar un producto específico: POST /cart/remove/:id
+router.post('/remove/:id', cartController.removeItem);
+
 // La ruta real será POST /cart/clear
 router.post('/clear', cartController.clearCart);
 
