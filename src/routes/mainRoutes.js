7c47d738a1848fa42d1eee079110d4edc93209e5
jennaguilar <ajenniferlucia@gmail.com>
2026-05-02 01:28:10 -0300

    feat: Se agrego la ruta dinámica /categories/:category.

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -18,6 +18,8 @@ router.get('/profile', mainController.getProfile);
 router.get('/register', mainController.getRegister);
 // La ruta real será GET /search?query=...
 router.get('/search', mainController.getSearch);
+// Ruta dinámica para categorías: GET /categories/:category
+router.get('/categories/:category', mainController.getCategory);
 
 // --- DEFINICIÓN DE RUTAS POST ---
 // Esta ruta tiene 3 partes: la URL, un array de middlewares de validación, y el método del controlador.
