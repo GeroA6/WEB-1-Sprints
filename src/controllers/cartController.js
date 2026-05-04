de87bcffad82f2f1867a4b4399105437fdb5232d
Pablo Aguilar <paedmaag@gmail.com>
2026-05-03 23:04:44 -0300

    Fix: se redefinió el módulo cartController debido a previos conflictos al hacer Merges en los diferentes Branches

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,4 +1,4 @@
-// importamos el servicio de carrito que acabamos de crear
+// importamos el servicio de carrito
 const cartService = require('../services/cartService');
 // Importamos el servicio de productos para utilizar la función normalizeId
 const productService = require('../services/productService');
@@ -6,13 +6,7 @@ const productService = require('../services/productService');
 const cartController = {
     
     // ESCENARIO 1: Agregar al carrito
-    addCart: (req, res) => {
-        const productId = req.params.id; // Obtenemos el id del producto desde la ruta (ej: /cart/add/2)
-        // Le pasamos la sesión entera y el id al servicio para que haga la lógica
-        cartService.addProduct(req.session, productId);
-
-        // Redirigimos al usuario a la página del carrito para que vea lo que agregó
-        res.redirect('/cart');
+    addCart: (req, res, next) => {
         try {
             // Normalizamos y validamos el ID antes de agregarlo al carrito
             const productId = productService.normalizeId(req.params.id);
@@ -24,13 +18,12 @@ const cartController = {
         }
     },
 
-    // ESCENARIO 2: Ver el Carrito
+    // ESCENARIO 2 y 5: Ver el Carrito
     viewCart: (req, res) => {
-        
         // El servicio nos devuelve el carrito detallado y el total ya calculado
         const { cartWithDetails, total } = cartService.getCartDetails(req.session);
 
-        // Le mandamos los datos al EJS para que los muestre en la vista (controlador -> vista) 
+        // Le mandamos los datos al EJS para que los muestre en la vista
         res.render('pages/cart', { 
             cart: cartWithDetails, 
             total: total,
@@ -46,14 +39,7 @@ const cartController = {
     },
 
     // ESCENARIO 3: Modificar cantidad (+ o -)
-    updateCart: (req, res) => {
-        const productId = req.params.id;
-        // Obtenemos la acción desde la query string (ej: /cart/update/2?action=increase)
-        const action = req.query.action; 
-
-        cartService.updateQuantity(req.session, productId, action);
-        
-        res.redirect('/cart');
+    updateCart: (req, res, next) => {
         try {
             // Validamos el ID
             const productId = productService.normalizeId(req.params.id);
@@ -67,12 +53,6 @@ const cartController = {
     },
 
     // Quitar un producto específico
-    removeItem: (req, res) => {
-        const productId = req.params.id;
-
-        cartService.removeProduct(req.session, productId);
-        
-        res.redirect('/cart');
     removeItem: (req, res, next) => {
         try {
             // Validamos el ID
@@ -84,7 +64,6 @@ const cartController = {
             next(error);
         }
     }
-}
 };
 
 module.exports = cartController;
