16bf0aae2b6799b81c7906cfa035cb8f5d071e2a
Pablo Aguilar <paedmaag@gmail.com>
2026-05-15 18:24:01 -0300

    Revert "refactor(models): migrar productModel a SQLite para US4 del carrito"
    This reverts commit d82c41e6bc20764912af323e52c027cc08cda184.

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -1,18 +1,25 @@
-const db = require('../db/database');
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/products.json');
 
 const productModel = {
+    // El método privado para leer el archivo
+    _readJson: function() {
+        const productsJSON = fs.readFileSync(productsFilePath, 'utf-8');
+        return JSON.parse(productsJSON);
+    },
+
     // Devuelve todos los productos
     findAll: function() {
-        // Usamos .all() para traer todas las filas de la tabla products
-        const query = db.prepare('SELECT * FROM products');
-        return query.all();
+        return this._readJson();
     },
 
     // Busca uno solo por ID
     findById: function(id) {
-        // Usamos .get() con un parámetro (?) para traer un único producto de forma segura
-        const query = db.prepare('SELECT * FROM products WHERE id = ?');
-        return query.get(id);
+        const products = this.findAll();
+        // Usamos == para que compare string con number sin problemas
+        return products.find(product => product.id == id);
     }
     
 };
